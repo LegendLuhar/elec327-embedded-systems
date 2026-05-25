84c60128f2fe68e70664ad5b99fbd7cf6d9603c0
Rahul Kishore <rahul.kishorez@gmail.com>
2026-05-25 14:02:58 -0500

    Add files via upload

---
+++ Lab5/Debug/subdir_vars.mk
@@ -0,0 +1,39 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../mspm0g3507.cmd 
+
+C_SRCS += \
+../hw_interface.c \
+../lab5.c \
+../startup_mspm0g350x_ticlang.c 
+
+C_DEPS += \
+./hw_interface.d \
+./lab5.d \
+./startup_mspm0g350x_ticlang.d 
+
+OBJS += \
+./hw_interface.o \
+./lab5.o \
+./startup_mspm0g350x_ticlang.o 
+
+OBJS__QUOTED += \
+"hw_interface.o" \
+"lab5.o" \
+"startup_mspm0g350x_ticlang.o" 
+
+C_DEPS__QUOTED += \
+"hw_interface.d" \
+"lab5.d" \
+"startup_mspm0g350x_ticlang.d" 
+
+C_SRCS__QUOTED += \
+"../hw_interface.c" \
+"../lab5.c" \
+"../startup_mspm0g350x_ticlang.c" 
+
+
