aaecf1980515875d378a24f9527859fe40a0944b
Rahul Kishore <rahul.kishorez@gmail.com>
2026-05-25 14:05:08 -0500

    Add files via upload

---
+++ Lab6/Debug/buttons.d
@@ -0,0 +1,65 @@
+# FIXED
+
+buttons.o: ../buttons.c ../buttons.h ../delay.h \
+ /Users/rahul/ti/mspm0_sdk_2_09_00_01/source/ti/devices/msp/msp.h \
+ /Users/rahul/ti/mspm0_sdk_2_09_00_01/source/ti/devices/DeviceFamily.h \
+ /Users/rahul/ti/mspm0_sdk_2_09_00_01/source/ti/devices/msp/m0p/mspm0g350x.h \
+ /Users/rahul/ti/mspm0_sdk_2_09_00_01/source/third_party/CMSIS/Core/Include/core_cm0plus.h \
+ /Users/rahul/ti/mspm0_sdk_2_09_00_01/source/ti/devices/msp/peripherals/hw_adc12.h \
+ /Users/rahul/ti/mspm0_sdk_2_09_00_01/source/ti/devices/msp/peripherals/hw_aes.h \
+ /Users/rahul/ti/mspm0_sdk_2_09_00_01/source/ti/devices/msp/peripherals/hw_comp.h \
+ /Users/rahul/ti/mspm0_sdk_2_09_00_01/source/ti/devices/msp/peripherals/hw_crc.h \
+ /Users/rahul/ti/mspm0_sdk_2_09_00_01/source/ti/devices/msp/peripherals/hw_dac12.h \
+ /Users/rahul/ti/mspm0_sdk_2_09_00_01/source/ti/devices/msp/peripherals/hw_dma.h \
+ /Users/rahul/ti/mspm0_sdk_2_09_00_01/source/ti/devices/msp/peripherals/hw_flashctl.h \
+ /Users/rahul/ti/mspm0_sdk_2_09_00_01/source/ti/devices/msp/peripherals/hw_gpio.h \
+ /Users/rahul/ti/mspm0_sdk_2_09_00_01/source/ti/devices/msp/peripherals/hw_gptimer.h \
+ /Users/rahul/ti/mspm0_sdk_2_09_00_01/source/ti/devices/msp/peripherals/hw_i2c.h \
+ /Users/rahul/ti/mspm0_sdk_2_09_00_01/source/ti/devices/msp/peripherals/hw_iomux.h \
+ /Users/rahul/ti/mspm0_sdk_2_09_00_01/source/ti/devices/msp/peripherals/hw_mathacl.h \
+ /Users/rahul/ti/mspm0_sdk_2_09_00_01/source/ti/devices/msp/peripherals/hw_mcan.h \
+ /Users/rahul/ti/mspm0_sdk_2_09_00_01/source/ti/devices/msp/peripherals/hw_oa.h \
+ /Users/rahul/ti/mspm0_sdk_2_09_00_01/source/ti/devices/msp/peripherals/hw_rtc.h \
+ /Users/rahul/ti/mspm0_sdk_2_09_00_01/source/ti/devices/msp/peripherals/hw_spi.h \
+ /Users/rahul/ti/mspm0_sdk_2_09_00_01/source/ti/devices/msp/peripherals/hw_trng.h \
+ /Users/rahul/ti/mspm0_sdk_2_09_00_01/source/ti/devices/msp/peripherals/hw_uart.h \
+ /Users/rahul/ti/mspm0_sdk_2_09_00_01/source/ti/devices/msp/peripherals/hw_vref.h \
+ /Users/rahul/ti/mspm0_sdk_2_09_00_01/source/ti/devices/msp/peripherals/hw_wuc.h \
+ /Users/rahul/ti/mspm0_sdk_2_09_00_01/source/ti/devices/msp/peripherals/hw_wwdt.h \
+ /Users/rahul/ti/mspm0_sdk_2_09_00_01/source/ti/devices/msp/peripherals/m0p/hw_factoryregion.h \
+ /Users/rahul/ti/mspm0_sdk_2_09_00_01/source/ti/devices/msp/peripherals/m0p/hw_cpuss.h \
+ /Users/rahul/ti/mspm0_sdk_2_09_00_01/source/ti/devices/msp/peripherals/m0p/hw_debugss.h \
+ /Users/rahul/ti/mspm0_sdk_2_09_00_01/source/ti/devices/msp/peripherals/m0p/hw_sysctl.h \
+ /Users/rahul/ti/mspm0_sdk_2_09_00_01/source/ti/devices/msp/peripherals/m0p/sysctl/hw_sysctl_mspm0g1x0x_g3x0x.h
+../buttons.h:
+../delay.h:
+/Users/rahul/ti/mspm0_sdk_2_09_00_01/source/ti/devices/msp/msp.h:
+/Users/rahul/ti/mspm0_sdk_2_09_00_01/source/ti/devices/DeviceFamily.h:
+/Users/rahul/ti/mspm0_sdk_2_09_00_01/source/ti/devices/msp/m0p/mspm0g350x.h:
+/Users/rahul/ti/mspm0_sdk_2_09_00_01/source/third_party/CMSIS/Core/Include/core_cm0plus.h:
+/Users/rahul/ti/mspm0_sdk_2_09_00_01/source/ti/devices/msp/peripherals/hw_adc12.h:
+/Users/rahul/ti/mspm0_sdk_2_09_00_01/source/ti/devices/msp/peripherals/hw_aes.h:
+/Users/rahul/ti/mspm0_sdk_2_09_00_01/source/ti/devices/msp/peripherals/hw_comp.h:
+/Users/rahul/ti/mspm0_sdk_2_09_00_01/source/ti/devices/msp/peripherals/hw_crc.h:
+/Users/rahul/ti/mspm0_sdk_2_09_00_01/source/ti/devices/msp/peripherals/hw_dac12.h:
+/Users/rahul/ti/mspm0_sdk_2_09_00_01/source/ti/devices/msp/peripherals/hw_dma.h:
+/Users/rahul/ti/mspm0_sdk_2_09_00_01/source/ti/devices/msp/peripherals/hw_flashctl.h:
+/Users/rahul/ti/mspm0_sdk_2_09_00_01/source/ti/devices/msp/peripherals/hw_gpio.h:
+/Users/rahul/ti/mspm0_sdk_2_09_00_01/source/ti/devices/msp/peripherals/hw_gptimer.h:
+/Users/rahul/ti/mspm0_sdk_2_09_00_01/source/ti/devices/msp/peripherals/hw_i2c.h:
+/Users/rahul/ti/mspm0_sdk_2_09_00_01/source/ti/devices/msp/peripherals/hw_iomux.h:
+/Users/rahul/ti/mspm0_sdk_2_09_00_01/source/ti/devices/msp/peripherals/hw_mathacl.h:
+/Users/rahul/ti/mspm0_sdk_2_09_00_01/source/ti/devices/msp/peripherals/hw_mcan.h:
+/Users/rahul/ti/mspm0_sdk_2_09_00_01/source/ti/devices/msp/peripherals/hw_oa.h:
+/Users/rahul/ti/mspm0_sdk_2_09_00_01/source/ti/devices/msp/peripherals/hw_rtc.h:
+/Users/rahul/ti/mspm0_sdk_2_09_00_01/source/ti/devices/msp/peripherals/hw_spi.h:
+/Users/rahul/ti/mspm0_sdk_2_09_00_01/source/ti/devices/msp/peripherals/hw_trng.h:
+/Users/rahul/ti/mspm0_sdk_2_09_00_01/source/ti/devices/msp/peripherals/hw_uart.h:
+/Users/rahul/ti/mspm0_sdk_2_09_00_01/source/ti/devices/msp/peripherals/hw_vref.h:
+/Users/rahul/ti/mspm0_sdk_2_09_00_01/source/ti/devices/msp/peripherals/hw_wuc.h:
+/Users/rahul/ti/mspm0_sdk_2_09_00_01/source/ti/devices/msp/peripherals/hw_wwdt.h:
+/Users/rahul/ti/mspm0_sdk_2_09_00_01/source/ti/devices/msp/peripherals/m0p/hw_factoryregion.h:
+/Users/rahul/ti/mspm0_sdk_2_09_00_01/source/ti/devices/msp/peripherals/m0p/hw_cpuss.h:
+/Users/rahul/ti/mspm0_sdk_2_09_00_01/source/ti/devices/msp/peripherals/m0p/hw_debugss.h:
+/Users/rahul/ti/mspm0_sdk_2_09_00_01/source/ti/devices/msp/peripherals/m0p/hw_sysctl.h:
+/Users/rahul/ti/mspm0_sdk_2_09_00_01/source/ti/devices/msp/peripherals/m0p/sysctl/hw_sysctl_mspm0g1x0x_g3x0x.h:
---
+++ Lab6/Debug/subdir_vars.mk
@@ -0,0 +1,69 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../mspm0g3507.cmd 
+
+C_SRCS += \
+../buttons.c \
+../buzzer.c \
+../delay.c \
+../lab6.c \
+../leds.c \
+../startup_mspm0g350x_ticlang.c \
+../state_machine.c \
+../timing.c 
+
+C_DEPS += \
+./buttons.d \
+./buzzer.d \
+./delay.d \
+./lab6.d \
+./leds.d \
+./startup_mspm0g350x_ticlang.d \
+./state_machine.d \
+./timing.d 
+
+OBJS += \
+./buttons.o \
+./buzzer.o \
+./delay.o \
+./lab6.o \
+./leds.o \
+./startup_mspm0g350x_ticlang.o \
+./state_machine.o \
+./timing.o 
+
+OBJS__QUOTED += \
+"buttons.o" \
+"buzzer.o" \
+"delay.o" \
+"lab6.o" \
+"leds.o" \
+"startup_mspm0g350x_ticlang.o" \
+"state_machine.o" \
+"timing.o" 
+
+C_DEPS__QUOTED += \
+"buttons.d" \
+"buzzer.d" \
+"delay.d" \
+"lab6.d" \
+"leds.d" \
+"startup_mspm0g350x_ticlang.d" \
+"state_machine.d" \
+"timing.d" 
+
+C_SRCS__QUOTED += \
+"../buttons.c" \
+"../buzzer.c" \
+"../delay.c" \
+"../lab6.c" \
+"../leds.c" \
+"../startup_mspm0g350x_ticlang.c" \
+"../state_machine.c" \
+"../timing.c" 
+
+
